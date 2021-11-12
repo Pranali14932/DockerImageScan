3cf620ab0ea8f70ef37a6cd8d469f3e555e5a089
Pranali14932 <92152803+Pranali14932@users.noreply.github.com>
2021-11-11 20:58:05 -0800

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,20 +9,32 @@
 // }
 
 
-public class Hello implements Runnable
-{
-public void run()
-{  
-  System.out.println(Thread.currentThread() + " is a daemon thread");  
-}  
-public static void main(String[] args) 
-{
-Hello obj = new Hello();
-Thread t1 = new Thread(obj, "Thread 1");
-
-  t1.start(); 
-  t1.setDaemon(true); 
+// public class Hello implements Runnable
+// {
+// public void run()
+// {  
+//   System.out.println(Thread.currentThread() + " is a daemon thread");  
+// }  
+// public static void main(String[] args) 
+// {
+// Hello obj = new Hello();
+// Thread t1 = new Thread(obj, "Thread 1");
+//  t1.setDaemon(true); 
+// for(int i=0;;i++)
+//   t1.start(); 
+ 
   
-  System.out.println("Main thread ending"); 
- }
-}
+//   System.out.println("Main thread ending"); 
+//  }
+// }
+public class Hello {  
+	
+	 public static void main(String[] args){  
+	int x;
+	for(int i=0;;i++) {
+		x=i;
+		System.out.println(x+x);
+	}
+	 
+	 }  
+	}  
