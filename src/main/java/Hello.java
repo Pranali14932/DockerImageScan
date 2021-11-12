d7e91c202eaafc40a8ddea5b5f39926e768a23b0
Pranali14932 <92152803+Pranali14932@users.noreply.github.com>
2021-11-11 20:26:51 -0800

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,28 @@
 
-public class Hello {
+// public class Hello {
   
-  public static String GREETING = "Hello world!";
+//   public static String GREETING = "Hello world!";
   
-  public static void main(String []args) {
+//   public static void main(String []args) {
     
-  }  
-}
+//   }  
+// }
 
+
+public class Hello implements Runnable
+{
+public void run()
+{  
+  System.out.println(Thread.currentThread() + " is a daemon thread");  
+}  
+public static void main(String[] args) 
+{
+Hello obj = new Hello();
+Thread t1 = new Thread(obj, "Thread 1");
+
+  t1.start(); 
+  t1.setDaemon(true); 
+  
+  System.out.println("Main thread ending"); 
+ }
+}
